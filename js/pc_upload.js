6016066424b3b8b596a9e921ef9219733a312943
shindouhiro <274173368@qq.com>
2014-12-05 18:13:12 +0800

    12-05

--- js/pc_upload.js
+++ js/pc_upload.js
@@ -6,8 +6,8 @@
  			navigator.camera.getPicture(onSuccess1, onFail, { 
 				quality: 70,
 			    destinationType: Camera.DestinationType.FILE_URL, //以文件地址返回url
-			    sourceType:Camera.PictureSourceType.Camera,
-			   //  sourceType:Camera.PictureSourceType.PHOTOLIBRARY,
+			    sourceType:Camera.PictureSourceType.PHOTOLIBRARY,
+			    // sourceType:Camera.PictureSourceType.Camera,
 			   // mediaType:Camera.MediaType.VIDEO,
 			}); 			
  		})
@@ -23,8 +23,8 @@
  	
 	}
 	function onSuccess1(imageURI){
-		$condition=$("#pc_idens").val();
-		if($condition==""){
+		$condition=$(".pc_idens").attr('src');
+		if($condition==undefined){
 		$width=$("#content").width();
 		$img="<img src='"+imageURI+"' width='"+$width+"' height='100' class='pc_idens'/>";
 		$(".pc_iden").append($img);
@@ -37,8 +37,8 @@
 	}
 	
 	function onSuccess2(imageURI){	
-		$condition=$("#pc_offs").val();
-		if($condition==""){
+		$condition=$(".pc_offs").attr('src');
+		if($condition==undefined){
 		$width=$("#content").width();
 		$img="<img src='"+imageURI+"' width='"+$width+"' height='100' class='pc_offs'/>";
 		$(".pc_off").append($img);
@@ -53,31 +53,38 @@
 $(function(){
 	$("#pc_submit").click(function(){
 		
-		var imageURI=$("#pc_idens").val();
-		 var options = new FileUploadOptions();
-          options.fileKey="file";
-           options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-           options.mimeType="image/jpeg";
-            var params = {};
-            params.name = "iden";
-           options.params = params;
-
-           var ft = new FileTransfer();
-           ft.upload(imageURI, encodeURI("http://yongxinghua.is-great.net/upload.php"), win, fail, options);
-           
-           var imageURI=$("#pc_offs").val();
+		    var imageURI=$(".pp_idens").attr('src');
+		    var options = new FileUploadOptions();
+            options.fileKey="url";
+            var imageURI2=$(".pc_offs").attr('src');
             var options = new FileUploadOptions();
-          options.fileKey="file";
-           options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-           options.fileName+='.jpg';
-           options.mimeType="image/jpeg";
+            options.fileKey="url2";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.fileName=imageURI2.substr(imageURI2.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
             var params = {};
-            params.name = "off";
-           options.params = params;
+            var id = getItem('u_id');//获取userid
+            alert(id);
+            var p_principal=$("#c_principal").val();// 获取负责人
+            var p_email=$("#c_email").val();//获取邮箱
+            var p_iden=$("#c_iden").val();//获取身份证号码
+            var p_type=$("#c_type").val();//获取公司类型
+            var p_province=$("#c_province").val();//获取省
+            var p_city=$("#c_city").val();//获取市
+            var p_area=$("#c_area").val();//获取区
+            params.userid = id;
+            params.c_principal= c_principal;
+            params.c_email= c_email;
+            params.c_iden=c_iden;
+            params.c_type=c_type;
+            params.c_province=c_province;
+            params.c_city=c_city;
+            params.c_area=c_area;
+           	options.params = params;
 
-           var ft = new FileTransfer();
-           ft.upload(imageURI, encodeURI("http://yongxinghua.is-great.net/upload.php"), win, fail, options);
-          
+            var ft = new FileTransfer();
+            ft.upload(imageURI, encodeURI("http://www.29mins.com/mobile_admin/mobile_login/update_account_2url"), win, fail, options);
+           
 	})
 	 function win(r) {
 			alert("上传成功");
@@ -90,19 +97,19 @@ $(function(){
         }
         
         
-     $("#content").on("longTap","img",function(){
-  	
-			if(confirm("确定删除")){
-			$(this).remove();
-			$id=$(this).attr("class");
- 			$("#"+$id).val("");
-				return true;
-				}
-				return false;
-
-
- 		
- 	 })
+    // $("#content").on("longTap","img",function(){
+//  	
+//			if(confirm("确定删除")){
+//			$(this).remove();
+//			$id=$(this).attr("class");
+// 			$("#"+$id).val("");
+//				return true;
+//				}
+//				return false;
+//
+//
+// 		
+// 	 })
     
 })	
 
--- js/pp_upload.js
+++ js/pp_upload.js
@@ -13,11 +13,12 @@
  		})
 	}
 	function onSuccess1(imageURI){
-		$condition=$("#pp_idens").val();
-		if($condition==""){
+		$condition=$(".pp_idens").attr('src');
+		if($condition==undefined){
 		$width=$("#content").width();
 		$img="<img src='"+imageURI+"' width='"+$width+"' height='100' class='pp_idens'/>";
 		$(".pp_iden").append($img);
+		alert($img);
 		$("#pp_idens").val(imageURI);
 		}else{
 			alert("只允许上传一张图片");
@@ -32,14 +33,13 @@
 $(function(){
 	$("#pp_submit").click(function(){
 		
-		var imageURI=$("#pp_idens").val();
+		var imageURI=$(".pp_idens").attr('src');
 		 var options = new FileUploadOptions();
           options.fileKey="url";
            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
            options.mimeType="image/jpeg";
             var params = {};
             var id = getItem('u_id');//获取userid
-            alert(id);
             var p_principal=$("#p_principal").val();// 获取负责人
             var p_email=$("#p_email").val();//获取邮箱
             var p_iden=$("#p_iden").val();//获取身份证号码
@@ -57,21 +57,64 @@ $(function(){
            var ft = new FileTransfer();
            ft.upload(imageURI, encodeURI("http://www.29mins.com/mobile_admin/mobile_login/update_account_url"), win, fail, options);
            var url2='http://www.29mins.com/mobile_admin/mobile_login/get_account_credit?userid='+id;
-           alert(id);
-           alert(url2);
+        
            //获取状态值
-		   $.get(url2,function(data){
-					obj = JSON.parse(data);
+		  var url='http://www.29mins.com/mobile_admin/mobile_login/get_account_credit?userid='+id;
+				// alert(url);
+				$.get(url,function(data){
+					 obj = JSON.parse(data);
+					var province_id=obj.province_id;
+					var city_id=obj.city_id;
+					var area_id=obj.area_id;
 					var state_id=obj.state_id;
+					var card_id=obj.card_id;
+					var name=obj.name;
+					var email=obj.email;
 					var card_img='http://www.29mins.com/'+obj.url.url;
-					var url2='http://www.29mins.com/mobile_admin/mobile_login/get_state_name?state_id='+state_id;
-					$.get(url2,function(data){
+					var url='http://www.29mins.com/mobile_admin/mobile_login/get_province_name?province_id='+province_id; 
+					$.get(url,function(data){
+						obj = JSON.parse(data);
+						var province_name=obj.name;
+						setItem('pp_province_name',province_name);
+					})
+					var url='http://www.29mins.com/mobile_admin/mobile_login/get_city_name?city_id='+city_id;
+					$.get(url,function(data){
+						obj = JSON.parse(data);
+						var city_name=obj.name;
+						setItem('pp_city_name',city_name);
+					})
+					var url='http://www.29mins.com/mobile_admin/mobile_login/get_area_name?area_id='+area_id;
+					$.get(url,function(data){
+						obj = JSON.parse(data);
+						var area_name=obj.name;
+						setItem('pp_area_name',area_name);
+					})
+					var url='http://www.29mins.com/mobile_admin/mobile_login/get_state_name?state_id='+state_id;
+					$.get(url,function(data){
 						obj = JSON.parse(data);
 						var state_name=obj.name;
-						alert(state_name);
-						$.ui.loadContent("#audit");
+						setItem('pp_state_name',state_name);
 					})
-			})
+			
+					var state_name=getItem('pp_state_name');
+					var area_name=getItem('pp_area_name');
+					var city_name=getItem('pp_city_name');
+					var province_name=getItem('pp_province_name');
+					// alert(province_name);
+					var str="";
+					str+='<li>'+state_name+'</li>';
+					str+='<li>负责人:'+name+'</li>';
+					str+='<li>邮箱:'+email+'</li>';
+					str+='<li>身份证:'+card_id+'</li>';
+					str+='<li>'+province_name+'省</li>';
+					str+='<li>'+city_name+'市</li>';
+					str+='<li>'+area_name+'</li>';
+					str+='<li><img src="'+card_img+'"/></li>';
+					alert(str);
+					$("#audi").append(str);
+					$.ui.loadContent("#audit");
+
+				})
 			
 				
 	})
@@ -86,15 +129,13 @@ $(function(){
         }
         
         
-     $("#content").on("longTap","img",function(){
-  	
+     $("#provider_personage").on("longTap","img",function(){
 			if(confirm("确定删除")){
 			$(this).remove();
-			$id=$(this).attr("class");
- 			$("#"+$id).val("");
 				return true;
-				}
+				}else{
 				return false;
+				}
 
 
  		
